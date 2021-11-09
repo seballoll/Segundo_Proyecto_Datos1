d5e607ec52305c1141f68d0c8092fc99414fed45
Andrea Fernández <andreafernez@estudiantec.cr>
2021-11-09 02:48:09 -0600

    html y js
    página web

---
+++ Calculator/src/main/jscripttt.js
@@ -0,0 +1,9 @@
+function multiplicar (){
+    document.getElementById("nombredelacaja").value+="*";
+
+} // para insertar símbolo de multiplicar en la entrada de texto
+
+function dividir (){
+    document.getElementById("nombredelacaja").value+="/";
+
+} // para insertar símbolo de restar en la entrada de texto
