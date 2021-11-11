e8cc5bca4833f28a60176d25463d134ae6f127da
Andrea Fernández <andreafernez@estudiantec.cr>
2021-11-11 00:59:23 -0600

    arbol binario

--- CalculatorApp/src/main/java/com/sample/arbol/arbolcalc.java
+++ CalculatorApp/src/main/java/com/sample/arbol/arbolcalc.java
@@ -1,84 +1,167 @@
 package com.sample.arbol;
-import java.util.Stack;
+import java.util.*;
 
 public class arbolcalc {
+    private class nodo {
+        private char contenido;
+        private nodo left;
+        private nodo right;
 
-    public boolean operador(String operator) {
-        return (operator.equals("+")  ||operator.equals("/")
-                || operator.equals("-") || operator.equals("+")
-                || operator.equals("*") ||
-                operator.equals("%"));
+        private boolean isLeaf() {
+            return (left == null && right == null);
+        }
     }
 
-    public nodo constructorTree(String postorderExpression) {
+    private nodo root;
+    private Scanner in;
 
-        String TempNum ="";
-
-        Stack<nodo> pila = new Stack<>();
-
-        String caracterEvaluado;
-
-        for (int i=0; i<postorderExpression.length();i++) {
-
-            caracterEvaluado = Character.toString(postorderExpression.charAt(i));
-
-            if (caracterEvaluado.equals(" ")) {
-                pila.add(new nodo(TempNum));
-                TempNum="";
-                continue;
-            }
-
-            if (operador(caracterEvaluado)) {
-
-                nodo right = pila.pop();
-                nodo left = pila.pop();
-
-                nodo node = new nodo(caracterEvaluado, left, right);
-
-                pila.add(node);
-
-            }
-            else if (caracterEvaluado.equals(" ")) {
-
-                pila.add(new nodo(TempNum));
-                TempNum="";
-                continue;
-            }
-
-            else {
-                TempNum = TempNum + caracterEvaluado;
-            }
-        }
-
-        return pila.peek();
+    public arbolcalc() {
+        root = null;
+        in = new Scanner(System.in);
     }
 
-    public double Resultado(nodo root) {
-        double respuesta = 0;
-
-        if (root.left==null && root.right==null){
-            return Double.parseDouble(root.data);
+    public void inorderPrint() {
+        if (root != null) {
+            inorderPrintTree(root);
         }
-
-        switch (root.data){
-
-            case "+":
-                respuesta = Resultado(root.left) + Resultado(root.right);
-                break;
-            case "-":
-                respuesta = Resultado(root.left) - Resultado(root.right);
-                break;
-            case "*":
-                respuesta = Resultado(root.left) * Resultado(root.right);
-                break;
-            case "/":
-                respuesta = Resultado(root.left) / Resultado(root.right);
-                break;
-            case "%":
-                respuesta = Resultado(root.left) % Resultado(root.right);
-                break;
-        }
-        return respuesta;
     }
 
+    private static void inorderPrintTree(nodo root) {
+        if (root.isLeaf())
+            System.out.print(root.contenido);
+        else {
+
+            System.out.print("(");
+            inorderPrintTree(root.left);
+            System.out.print(" " + root.contenido + " ");
+            inorderPrintTree(root.right);
+            System.out.print(")");
+        }
+    }
+
+    public void preorderPrint() {
+        if (root != null) {
+            preorderPrintTree(root);
+        }
+    }
+
+    private static void preorderPrintTree(nodo root) {
+        if (root.isLeaf()) {
+            System.out.print(root.contenido);
+        } else {
+
+            switch (root.contenido) {
+                case '+':
+                    System.out.print("sumar");
+                    break;
+                case '-':
+                    System.out.print("restar");
+                    break;
+                case '*':
+                    System.out.print("multiplicar");
+                    break;
+                case '/':
+                    System.out.print("dividir");
+                    break;
+                case '%':
+                    System.out.print("porcentaje");
+                    break;
+            }
+
+            System.out.print("(");
+            preorderPrintTree(root.left);
+            System.out.print(", ");
+            preorderPrintTree(root.right);
+            System.out.print(")");
+        }
+    }
+
+    public void postorderPrint() {
+        if (root != null) {
+            postorderPrintTree(root, 0);
+        }
+    }
+
+    private static void postorderPrintTree(nodo root, int margin) {
+        if (root.isLeaf()) {
+            printMargin(margin);
+            System.out.print(root.contenido + "  ");
+        } else {
+            postorderPrintTree(root.left, margin + 1);
+            postorderPrintTree(root.right, margin + 1);
+
+            printMargin(margin);
+            switch (root.contenido) {
+                case '+':
+                    System.out.print("añade encima de");
+                    break;
+                case '-':
+                    System.out.print("suma encima de");
+                    break;
+                case '*':
+                    System.out.print("multiplica encima de");
+                    break;
+                case '/':
+                    System.out.print("divide encima de");
+                    break;
+                case '%':
+                    System.out.print("saca el porcentaje de sobre");
+                    break;
+            }
+        }
+    }
+
+    private static void printMargin(int margin) {
+        System.out.println();
+        for (int i = 1; i <= margin; i++) {
+            System.out.print("    ");
+        }
+    }
+
+    public void read() {
+        root = readTree();
+    }
+
+    private nodo readTree() {
+        nodo n = new nodo();
+
+        char ch = in.findInLine("(\\S)").charAt(0);
+        if ((ch >= 'a') && (ch <='z')) {
+
+            n.contenido = ch;
+            n.left = null;
+            n.right = null;
+        } else if (ch == '(') {
+
+            n.left = readTree();
+            n.contenido = in.findInLine("(\\S)").charAt(0);
+            n.right = readTree();
+            ch = in.findInLine("(\\S)").charAt(0);
+            if (ch != ')') {
+                System.out.print("esperado ) - } asumido...");
+            }
+        } else {
+            System.out.print("esperado ( - error");
+            System.exit(1);
+        }
+
+        return n;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("\nescriba la expresión " +
+                "usando +,-,*,/, %");
+
+        arbolcalc tree = new arbolcalc();
+        tree.read();
+
+        System.out.println("\n* INFIX NOTATION:");
+        tree.inorderPrint();
+        System.out.print("\n\n* PREFIX NOTATION:\n");
+        tree.preorderPrint();
+        System.out.print("\n\n* POSTFIX NOTATION:");
+        tree.postorderPrint();
+        System.out.println();
+    }
 }
